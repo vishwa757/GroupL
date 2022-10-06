aaf4651cc1160d6cf139985a88d7b4d155f163e2
vishwa757 <114721540+vishwa757@users.noreply.github.com>
2022-10-06 17:39:53 +0530

    hi group mates check the first class
    hi
    
    Co-Authored-By: Govinda1414 <114895404+Govinda1414@users.noreply.github.com>
    Co-Authored-By: Mayurigunjal <114721569+Mayurigunjal@users.noreply.github.com>

---
+++ samplegithub/src/utility/Utilityclass.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class Utilityclass 
+{
+	public static void main (String [] args)
+	{
+		System.out.println("TestBase");
+		System.out.println("my first class");
+	}
+
+}
